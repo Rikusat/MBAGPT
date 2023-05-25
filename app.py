d5ad16eb6b6e68b6de3c1bffa3462ed9383ca78b
Rikusat <91641392+Rikusat@users.noreply.github.com>
2023-05-25 22:28:05 +0900

    Update app.py

--- app.py
+++ app.py
@@ -26,9 +26,8 @@ class OpenAIEmbeddings:
 
 
 
+
 # Load the Buffett and Branson databases
-buffettDB = Chroma(persist_directory=os.path.join('db', 'buffett'), embedding_function=embeddings)
-buffett_retriever = buffettDB.as_retriever(search_kwargs={"k": 3})
 
 bransonDB = Chroma(persist_directory=os.path.join('db', 'branson'), embedding_function=embeddings)
 branson_retriever = bransonDB.as_retriever(search_kwargs={"k": 3})
