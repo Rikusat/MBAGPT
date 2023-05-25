844791b3bf7b1697312c96da2791e9ad539a39da
Rikusat <91641392+Rikusat@users.noreply.github.com>
2023-05-25 22:11:05 +0900

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ openai.api_key = st.secrets.OpenAIAPI.openai_api_key
 st.header("MBAGPT: Chatting with Multiple Data Sources")
 
 # Initialize embedding
-embeddings = OpenAIEmbeddings()
+openai = OpenAIEmbeddings(openai_api_key="openai_api_key")
 
 # Load the Buffett and Branson databases
 buffettDB = Chroma(persist_directory=os.path.join('db', 'buffett'), embedding_function=embeddings)
